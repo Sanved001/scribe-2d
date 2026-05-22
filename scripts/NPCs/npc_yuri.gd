763b9b92ca7a63593461e0bcb8cceb194eb5eb78
sanved <166846665+Sanved001@users.noreply.github.com>
2026-05-22 20:59:07 +0530

    Map Expansion + Making the text readable

--- scripts/NPCs/npc_yuri.gd
+++ scripts/NPCs/npc_yuri.gd
@@ -6,4 +6,8 @@ var npc_dialog:Array[String] = [
 	"I want to eat cake all day long",
 	"uh.. who are you?",
 	"OH! You're the new traveler!",
+	"Congratulations on completing this map!",
+	"There's nothing ahead as of now",
+	"More things will be added to this tutorial map as features increase and time goes on",
+	"Well then, I'll see you in the next update!"
 	]
