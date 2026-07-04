57b708a7beb85bdb7d1ead1c5fd0bb52a9ba54ee
sanved <166846665+Sanved001@users.noreply.github.com>
2026-07-04 14:06:37 +0530

    Object_Respawn is half complete!

--- scenes/functions/object_respawn_root.gd
+++ scenes/functions/object_respawn_root.gd
@@ -1,11 +1,22 @@
 extends Node2D
+@onready var object_respawn: Node2D = $Object_Respawn
+@onready var object_rigid_body_2d_respawn: Node2D = $Object_RigidBody2D_Respawn
+
+
 @export var root_node:Node = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	SignalBus.Respawn_Object.connect(respawn_object)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func respawn_object(my_object:Node,hard_respawn:bool):
+	if not my_object is RigidBody2D:
+		object_respawn.respawn_object(my_object,hard_respawn)
+	elif my_object is RigidBody2D:
+		pass
