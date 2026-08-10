b007b30db565cdeba6e3099e7fd81c4428022007
sanved <166846665+Sanved001@users.noreply.github.com>
2026-08-10 21:47:01 +0530

    added level setter to level_0.gd, map expansion of test_level, removed unused variables from moving_door_6x3_1.gd, added a respawn function to player camera

--- scripts/player_camera.gd
+++ scripts/player_camera.gd
@@ -6,7 +6,8 @@ extends Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	SignalBus.Respawn.connect(Respawn)
+
 	if not lookahead_camera_enabled: 
 		self.position_smoothing_enabled = false
 		#self.drag_left_margin = 1.0
@@ -18,3 +19,6 @@ func _process(delta: float) -> void:
 		offset = offset.lerp(player.velocity, delta * lerp_speed)
 		#if offset.x >= 50: offset.x = 50
 		offset = offset.clamp(Vector2(-50,-50), Vector2(50,50))
+
+func Respawn(hard_respawn:bool):
+	self.global_position = player.global_position
